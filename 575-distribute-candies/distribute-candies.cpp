9304feaa83112d6607ce2b7517733b6fe5e31b30
Abhay Kumar Singh <148642793+Abhay52004@users.noreply.github.com>
2026-08-11 17:21:33 +0530

    Time: 29 ms (93.81%) | Memory: 85.6 MB (91.56%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& c) {
-        unordered_set<int> pq;
-        for(int i : c){
-            pq.insert(i);
+        sort(c.begin(),c.end());
+        int count = 1;
+        for(int i = 1 ; i<c.size() ; i++){
+            if(c[i] != c[i-1])
+            count++;
         }
-        return min(pq.size(),c.size()/2);
-        
+        if(count == c.size()/2)
+        return c.size()/2;
+
+       return min(count, (int)c.size() / 2);
     }
 };
